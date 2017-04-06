213ad281f9853eaad89bca9975925c83184d9b3d
Kyle Annen <kyleannen@gmail.com>
2017-04-05 20:17:05 -0500

    add materialize polls dropdown to nav

--- imports/containers/AppContainerRouter.jsx
+++ imports/containers/AppContainerRouter.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { Dropdown, NavItem, Button} from 'react-materialize';
 import {
 	BrowserRouter as Router,
 	Route,
@@ -14,16 +16,28 @@ import About from '../ui/About.jsx';
 
 export default class AppContainerRouter extends React.Component {
 	render() {
+		const dropdown = ReactDOM.findDOMNode(this.refs.dropdown)
+
+		$(dropdown).ready(function(){
+			$(".dropdown-button").dropdown();
+		})
 		return (
 			<Router>
 				<div>
+					
 					<nav className="blue">
 						<div className="nav-wrapper">
 							<Link className="brand-logo center" to="/">Polling</Link>
 							<ul id="mobile-nav" className="right hide-on-med-and-down">
 								<li><Link to="/">Home</Link></li>
 								<li><Link to="/about">About</Link></li>
+								<Dropdown trigger={<li id="poll-button"><Link to="#">Polls</Link></li>}>
+									<NavItem>Create Poll</NavItem>
+									<NavItem>My Polls</NavItem>
+									<NavItem>Browse Polls</NavItem>
+								</Dropdown>
 							</ul>
+
 							<AccountsUIWrapper className="float-xs-right"/>
 						</div>
 					</nav>
@@ -37,3 +51,9 @@ export default class AppContainerRouter extends React.Component {
 }
 
 
+// <ul id="dropdown1" ref="dropdown" class="dropdown-content">
+// 	<li><a href="#!">one</a></li>
+// 	<li><a href="#!">two</a></li>
+// 	<li class="divider"></li>
+// 	<li><a href="#!">three</a></li>
+// </ul>
